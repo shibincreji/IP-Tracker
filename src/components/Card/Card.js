0b3d450dae00fbb5f65fb05e2ed632873ae91472
Shibin C Reji <scr2404@gmail.com>
2023-02-21 12:50:10 +0530

    Update Card.js

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,4 +1,4 @@
-import React from "react";11111
+import React from "react";111111
 import { useGlobalContext } from "../../context";
 import Loading from "../../pages/loading/Loading";
 import { Wrapper } from "./Card.style";
