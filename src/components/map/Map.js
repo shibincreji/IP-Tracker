932689b6cfc4e86f512cc4b92daa61951b58137a
Shibin C Reji <scr2404@gmail.com>
2023-04-28 11:29:29 +0530

    Update Map.js

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useGlobalContext } from "../../context";
+import { useGlobalContext } from "../../context";1
 import { Wrapper } from "./Map.style";
 require("dotenv").config();
 
