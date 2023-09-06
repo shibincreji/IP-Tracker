bfe0f846431cc02116381828af9daaa9c3dc3172
Shibin C Reji <scr2404@gmail.com>
2023-09-06 12:23:45 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-11111111111import React from "react";
+111111111111import React from "react";
 import Searchbar from "../components/searchbar/Searchbar";
 import { Wrapper, Header } from "./Home.style";
 import Bg from "../assets/pattern-bg.png";
